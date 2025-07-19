b8ae479631ca4517ea51561b60eeba88c0bcc5e4
caio20538 <caio20538@gmail.com>
2025-07-18 21:04:21 -0300

    feat: update SecurityConfig to allow only ADMIN access to admin endpoint

--- userAuth/src/main/java/com/ms/userAuth/config/SecurityConfig.java
+++ userAuth/src/main/java/com/ms/userAuth/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers("/admin/**").hasAuthority("SCOPE_ADMIN")
                         .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
